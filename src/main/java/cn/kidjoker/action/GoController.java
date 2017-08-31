ff811d6bdd709f15c9ce344b7bd10eea6caee687
kidjoker <13621952184@163.com>
2017-09-01 00:22:10 +0800

    jason

---
+++ src/main/java/cn/kidjoker/action/GoController.java
@@ -0,0 +1,26 @@
+/** work for life!
+ * 
+ */
+package cn.kidjoker.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author kidjoker
+ *
+ * @date 2017年8月31日 
+ */
+@Controller
+@RequestMapping(value="/study")
+public class GoController {
+	
+	@RequestMapping(value="/index",method={RequestMethod.GET,RequestMethod.POST})
+	public String index(Model model) throws Exception {
+		model.addAttribute("ha","123");
+		return "index";
+	}
+	
+}
